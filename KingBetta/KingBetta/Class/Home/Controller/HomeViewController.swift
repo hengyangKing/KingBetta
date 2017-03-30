d0c03f2fc33f91c831a9a2a479c17f69b52d10dc
hengyangKing <hengyangKing.yeah.net>
2017-03-30 11:26:54 +0800

    add function

---
+++ KingBetta/KingBetta/Class/Home/Controller/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  KingBetta
+//
+//  Created by J on 2017/3/30.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoButtonClick(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func scanButtonClick(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func searchButtonClick(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func historyButtonClick(_ sender: UIBarButtonItem) {
+    }
+}
